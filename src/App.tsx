e86ebb114f8da38dcc0ac90f42fff4cdeceefdbc
eaysin-arafat <eaysin.arafat.j@gmail.com>
2024-04-28 20:09:36 +0600

    add information in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+{
+  /*
+author: eaysin arafat;
+date: 27/04/2024;
+description: It serves as a landing page for an ecommerce application, showcasing its features and offerings.
+*/
+}
+
 import HomePage from "./page/HomePage";
 
 function App() {
